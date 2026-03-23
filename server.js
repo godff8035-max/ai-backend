1eb5f32438709d7b96cb2b58d072e7d70b0fcdba
godff8035-max <godff8035@gmail.com>
2026-03-23 21:04:11 +0530

    Update API endpoint for content generation

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post("/generate", async (req, res) => {
     const { input } = req.body;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+     `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
